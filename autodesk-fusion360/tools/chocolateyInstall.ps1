7f71e99afb4abd969c51bb3ac32e2d25ec942a43
Steven Velez <sbv1976@gmail.com>
2013-10-23 10:56:41 -0400

    Initial commit
    This basically works, but before pushing it, I need to add support for updates
    (basically, clean up unused packages).  Committing now simply because I want
    to be able to test the icon url

---
+++ autodesk-fusion360/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+﻿$packageName = 'autodesk-fusion360'
+$processor = Get-WmiObject Win32_Processor
+$is64bit = $processor.AddressWidth -eq 64
+
+Import-Module C:\Chocolatey\chocolateyinstall\helpers\chocolateyInstaller.psm1
+#throw Exception
+
+if (!$is64bit) {
+    Write-ChocolateyFailure "$packageName" "Requires a 64-bit platform"
+} else {
+    $installerType = 'exe'
+    $url = '' # download url
+    $url64 = 'https://dl.appstreaming.autodesk.com/production/installers/Fusion%20360%20Client%20Downloader.exe'
+    $silentArgs = '-gm2 -! --quiet'
+    $validExitCodes = @(0) 
+
+    #Get-ChildItem env:
+
+    if (!$env:chocolateyInstallArguments) {
+        $env:chocolateyInstallArguments="-g"
+    }
+    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+    $env:chocolateyInstallArguments="-g -rp uninstall"
+    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+}
---
+++ autodesk-fusion360/tools/chocolateyUninstall.ps1
@@ -0,0 +1,30 @@
+﻿$packageName = 'autodesk-fusion360'
+$applicationId = '73e72ada57b7480280f7a6f4a289729f'
+$installStream = 'production'
+$silentArgs = '--quiet'
+$validExitCodes = @(0) 
+
+try {
+  $streamerdir = Join-Path "$env:ProgramFiles" "Autodesk\webdeploy\meta\streamer"
+  $res = Get-ChildItem $streamerdir | Sort-Object -Descending | ? { $_.BaseName -match "^\d{14}$" } |
+    % {Join-Path $_.FullName "streamer.exe" } | ?{ Test-Path $_ }
+  
+  if ($res) {
+    if ($res -is [system.array]) {
+        # we have an array, make it not...
+        $res = $res[0]
+    }
+
+    $uninstallargs="-g -p uninstall -s $installStream -a ${applicationId}"
+    $uninstallargs="$silentArgs $uninstallargs"
+    Start-ChocolateyProcessAsAdmin "$uninstallargs" "${res}" -validExitCodes $validExitCodes
+  } else {
+    throw "No installation Found"
+  }
+
+  # the following is all part of error handling
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
