8ff45fa0a3251e6b7adb95b5214504a36ca9597e
Steven Velez <sbv1976@gmail.com>
2013-10-28 21:17:59 -0400

    Rearranging to only elevate once
    Since the install is a two-step install and purge process, the previous approach was triggering UAC twice.
    This update combines the two steps in to a script that is granted elevation thereby only triggering UAC once.
    
    Still needs testing on a clean machine... some addition refactoring is pending.

--- autodesk-fusion360/tools/chocolateyInstall.ps1
+++ autodesk-fusion360/tools/chocolateyInstall.ps1
@@ -1,25 +1,34 @@
 ﻿$packageName = 'autodesk-fusion360'
-$processor = Get-WmiObject Win32_Processor
-$is64bit = $processor.AddressWidth -eq 64
+$url = '' # download url
+$url64 = 'https://dl.appstreaming.autodesk.com/production/installers/Fusion%20360%20Client%20Downloader.exe'
 
-Import-Module C:\Chocolatey\chocolateyinstall\helpers\chocolateyInstaller.psm1
-#throw Exception
+# For debugging
+# clsImport-Module C:\Chocolatey\chocolateyinstall\helpers\chocolateyInstaller.psm1
 
-if (!$is64bit) {
-    Write-ChocolateyFailure "$packageName" "Requires a 64-bit platform"
-} else {
-    $installerType = 'exe'
-    $url = '' # download url
-    $url64 = 'https://dl.appstreaming.autodesk.com/production/installers/Fusion%20360%20Client%20Downloader.exe'
-    $silentArgs = '-gm2 -! --quiet'
-    $validExitCodes = @(0) 
+try {
 
-    #Get-ChildItem env:
-
-    if (!$env:chocolateyInstallArguments) {
-        $env:chocolateyInstallArguments="-g"
+    $commonDir = $(Convert-Path $( Join-Path $MyInvocation.MyCommand.Definition "../../Common") -ErrorAction SilentlyContinue)
+    if ("$commonDir" -eq "") {
+        $commonDir = $(Convert-Path $( Join-Path $MyInvocation.MyCommand.Definition "../../../Common"))
     }
-    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-    $env:chocolateyInstallArguments="-g -rp uninstall"
-    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+
+    $processor = Get-WmiObject Win32_Processor
+    $is64bit = $processor.AddressWidth -eq 64
+    if (!$is64bit) {
+        throw "Requires a 64-bit platform"
+    }
+
+    $dlfile = Join-Path "$(Split-Path $MyInvocation.MyCommand.Definition)" "${packageName}.exe"
+    Get-ChocolateyWebFile $package "$dlfile" $url -url64bit $url64
+
+    $psFile = Join-Path "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 'installandpurge.ps1'
+    Start-ChocolateyProcessAsAdmin "& `'$psFile`' `'$dlfile`' `'$packageName`'"
+
+} catch {
+    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+    throw
+} finally {
+    if (Test-Path "$dlfile") {
+        Remove-Item "$dlfile"
+    }
 }
---
+++ autodesk-fusion360/tools/installandpurge.ps1
@@ -0,0 +1,15 @@
+﻿$url = '' # download url
+$url64 = "https://dl.appstreaming.autodesk.com/production/installers/$($MyInvocation.UnboundArguments[0])%20Downloader.exe"
+$silentArgs = '-gm2 -! --quiet'
+$validExitCodes = @(0) 
+
+$dlfile=$MyInvocation.UnboundArguments[0]
+$package=$MyInvocation.UnboundArguments[1]
+
+
+if (!$env:chocolateyInstallArguments) {
+    $env:chocolateyInstallArguments="-g"
+}
+
+Install-ChocolateyInstallPackage "$package" 'exe' "$silentArgs" "$dlfile" -validExitCodes $validExitCodes
+Start-ChocolateyProcessAsAdmin "$silentArgs -g -rp uninstall" "$dlfile" -validExitCodes $validExitCodes
