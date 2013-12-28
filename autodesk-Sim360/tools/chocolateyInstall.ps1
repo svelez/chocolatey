f0d5c477075c4a89929ef837858fc79ed6f241c5
Steven Velez <sbv1976@gmail.com>
2013-12-28 14:22:28 -0500

    Adding new chocolatey package for supporting Sim 360
    This is based on the utilities created for the Fusion 360 Support

---
+++ autodesk-Sim360/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿# For debugging
+# Import-Module C:\Chocolatey\chocolateyinstall\helpers\chocolateyInstaller.psm1
+
+try {
+
+    $commonDir = $(Convert-Path $( Join-Path $MyInvocation.MyCommand.Definition "../../Common") -ErrorAction SilentlyContinue)
+    if ("$commonDir" -eq "") {
+        $commonDir = $(Convert-Path $( Join-Path $MyInvocation.MyCommand.Definition "../../../Common"))
+    }
+    $env:PSModulePath = $env:PSModulePath + ";" + $commonDir
+    Import-Module autodesk-shared
+
+    install-autodeskapp $MyInvocation  'autodesk-sim360' 'Sim 360'
+    
+    Write-ChocolateySuccess "$packageName"
+} catch {
+    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+    throw
+} 
