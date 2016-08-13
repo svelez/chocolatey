8f0d047ed138ebc75adecce690574f1e4f52ce49
Steven Velez <steven.velez@autodesk.com>
2016-08-13 18:17:35 -0400

    Move to using the admin installer
    This is to make providing a checksum more honest.  And if we are going
    to have to update the chocolatey public gallery every release, then
    we might as well use this.

--- autodesk-fusion360/tools/chocolateyInstall.ps1
+++ autodesk-fusion360/tools/chocolateyInstall.ps1
@@ -1,4 +1,11 @@
-﻿$modulepath = $(Join-Path $(Split-Path -Parent $MyInvocation.MyCommand.Definition) "autodesk-shared")
-Import-Module $modulepath
+﻿
 
-install-autodeskapp $MyInvocation  'autodesk-fusion360' 'Fusion 360' 
+$packageArgs = @{
+    packageName              = "autodesk-fusion360"
+    SilentArgs               = "--quiet"
+    Url64bit                 = "http://dl.appstreaming.autodesk.com/production/installers/Fusion%20360%20Admin%20Install.exe" 
+    Checksum64               = "99B56AC3CFD806650F18F226E7EE2BC99BD8081A9FA358F22C4A54D90CDC97F7" 
+    ChecksumType64           = "sha256"
+}
+
+Install-ChocolateyPackage @packageArgs
