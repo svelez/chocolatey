cfa398e8796a8e82250e9138f841dd083facfe76
Steven Velez <steven.velez@autodesk.com>
2016-08-13 23:48:49 -0400

    Force update of package
    By commiting an old version number to git... this should convince
    the build on appveyor to actually generate a new package.

--- autodesk-fusion360/tools/chocolateyInstall.ps1
+++ autodesk-fusion360/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
     packageName              = "autodesk-fusion360"
     SilentArgs               = "--quiet"
     Url64bit                 = "http://dl.appstreaming.autodesk.com/production/installers/Fusion%20360%20Admin%20Install.exe" 
-    Checksum64               = "99B56AC3CFD806650F18F226E7EE2BC99BD8081A9FA358F22C4A54D90CDC97F7"
+    Checksum64               = "UNSET"
     ChecksumType64           = "sha256"
 }
 
