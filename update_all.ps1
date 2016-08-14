993f6ced9c0bde89e99269baed2ab4b161303dbb
Steven Velez <steven.velez@autodesk.com>
2016-08-13 22:58:05 -0400

    Add automated build integration
    Work in progress, but basics in place.

---
+++ update_all.ps1
@@ -0,0 +1,22 @@
+﻿param($Name = $null)
+cd $PSScriptRoot
+
+if (Test-Path update_vars.ps1) { . ./update_vars.ps1 }
+
+$options = @{
+    Timeout = 10
+    Threads = 10
+    Push    = $false
+    Mail = if ($Env:mail_user) {
+        @{
+            To        = $Env:mail_user
+            Server    = 'smtp.gmail.com'
+            UserName  = $Env:mail_user
+            Password  = $Env:mail_pass
+            Port      = 587
+            EnableSsl = $true
+        }
+    } else {}
+}
+
+Update-AUPackages -Name $Name -Options $options | Export-CliXML update_info.xml
