ad930ad2b46bb57e76f9b450d98d21b6633f05c1
appveyor <appveyor>
2016-09-08 04:18:32 +0000

    UPDATE BOT: 1 packages updated

--- autodesk-fusion360/tools/chocolateyInstall.ps1
+++ autodesk-fusion360/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
     packageName              = "autodesk-fusion360"
     SilentArgs               = "--quiet"
     Url64bit                 = "http://dl.appstreaming.autodesk.com/production/installers/Fusion%20360%20Admin%20Install.exe" 
-    Checksum64               = "1f90742d675ccdb607eda1589f74fa67c741173733348ce7dbe0a5efe96bf952"
+    Checksum64               = "98ce93cd0025e4dda02e938dfee3cce55177470a85dbce54c03099e94de7d303"
     ChecksumType64           = "sha256"
 }
 
