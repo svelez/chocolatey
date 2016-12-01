e1413acedd606e89b0d38a79d2ef63d9efbb5331
Steven Velez <steven.velez@autodesk.com>
2016-12-01 02:03:37 -0500

    Update version since the old au version stopped doing that

--- autodesk-fusion360/tools/chocolateyInstall.ps1
+++ autodesk-fusion360/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-﻿
+
 
 $packageArgs = @{
     packageName              = "autodesk-fusion360"
     SilentArgs               = "--quiet"
     Url64bit                 = "http://dl.appstreaming.autodesk.com/production/installers/Fusion%20360%20Admin%20Install.exe" 
-    Checksum64               = "98ce93cd0025e4dda02e938dfee3cce55177470a85dbce54c03099e94de7d303"
+    Checksum64               = "2d3ae733f3f3e63e98484c844792c304c50cd22086a0862b8c891f2156f0e46d"
     ChecksumType64           = "sha256"
 }
 
