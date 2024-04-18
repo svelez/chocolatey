ca191dcbfed0b69fdcd293ef22f9eaca3f040aa8
Chocolatey <chocolatey@realdimensions.net>
2024-04-18 06:01:57 +0000

    AU: 1 updated - autodesk-fusion360 [skip ci] https://gist.github.com/c376b3b578cdb9c00b917acb071084dd/af6f070555311e6fde765c8c2b8cb9f2d5bedb9d

--- autodesk-fusion360/tools/chocolateyInstall.ps1
+++ autodesk-fusion360/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
     packageName              = "autodesk-fusion360"
     SilentArgs               = "--quiet"
     Url64bit                 = "https://dl.appstreaming.autodesk.com/production/installers/Fusion%20Admin%20Install.exe?mkt_tok=OTE4LUZPRC00MzMAAAGQ_4x1-g_485K2BzSeU80BgE9c7dq6ZbUdKzoZQeWHj1HByXSICCL14qZFCr22kP2P4rjETzGoCI7-ZMkE0qnOnij3C_crNvd4UblOq4ALb0-5fs2OPc9A"
-    Checksum64               = "131ead3ff1b84e154a11fd7321d90d8b010ec86dbc5401636303758ecce75fd9"
+    Checksum64               = "47e07adca3ee94e889dfa4503a1457e656db966fcb57bd54e95156d658212811"
     ChecksumType64           = "sha256"
 }
 
